6bae08a901ac27183dd62f0a817835946ea3a35e
QIOIZ1 <ziqiuqi98@gmail.com>
2026-07-13 14:35:10 +0800

    Create ycy-km.meta.js

---
+++ ycy-km.meta.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name 羊羊得意
+// @namespace http://tampermonkey.net/
+// @version 5.0.0
+// @description 小🐏向前冲
+// @author 🐏
+// @match *://avg.163.com/me/edit
+// @match *://wap.avg.163.com/me/edit
+// @match https://avg.163.com/enginejs/index.html*
+// @match https://avg.163.com/engine/index.html*
+// @match https://wap.avg.163.com/enginejs/index.html*
+// @match https://wap.avg.163.com/engine/index.html*
+// @match https://avg.163.com/game/detail/*
+// @match https://wap.avg.163.com/game/detail/*
+// @icon https://www.google.com/s2/favicons?sz=64&domain=163.com
+// @grant unsafeWindow
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_xmlhttpRequest
+// @updateURL   https://cdn.jsdelivr.net/gh/QIOIZ1/mz-cdn@main/ycy-km.meta.js
+// @downloadURL https://cdn.jsdelivr.net/gh/QIOIZ1/mz-cdn@main/ycy-km.user.js
+// @run-at document-start
+// ==/UserScript==
